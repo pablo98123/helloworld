506c813c75557b5609db8ca2294b9db1558d2fb3
unknown <ppicapietra@gmail.com>
2019-03-05 11:05:18 -0300

    added getters and setters to Dosctor

--- src/main/java/com/bonan/docrank/Entity/Doctor.java
+++ src/main/java/com/bonan/docrank/Entity/Doctor.java
@@ -6,6 +6,69 @@ public class Doctor {
 	private Integer registrationNumber;
 	private Establishment[] establishments;
 	private Specialty[] specialties;
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the lastname
+	 */
+	public String getLastname() {
+		return lastname;
+	}
+	/**
+	 * @param lastname the lastname to set
+	 */
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	/**
+	 * @return the registrationNumber
+	 */
+	public Integer getRegistrationNumber() {
+		return registrationNumber;
+	}
+	/**
+	 * @param registrationNumber the registrationNumber to set
+	 */
+	public void setRegistrationNumber(Integer registrationNumber) {
+		this.registrationNumber = registrationNumber;
+	}
+	/**
+	 * @return the establishments
+	 */
+	public Establishment[] getEstablishments() {
+		return establishments;
+	}
+	/**
+	 * @param establishments the establishments to set
+	 */
+	public void setEstablishments(Establishment[] establishments) {
+		this.establishments = establishments;
+	}
+	/**
+	 * @return the specialties
+	 */
+	public Specialty[] getSpecialties() {
+		return specialties;
+	}
+	/**
+	 * @param specialties the specialties to set
+	 */
+	public void setSpecialties(Specialty[] specialties) {
+		this.specialties = specialties;
+	}
+	
+	
+	
 	
 
 }
